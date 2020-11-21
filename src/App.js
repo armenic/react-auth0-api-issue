998e0b8084d538e0d756a07b131b0981439c080c
Edgar Manukyan <edgar.manukyan@roche.com>
2020-11-20 22:45:36 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './Login';
+import LoginButton from "./Login";
+import LogoutButton from "./Logout";
+import Profile from "./Profile";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <LoginButton/>
+        <LogoutButton/>
+        <Profile/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Auth0Provider
+        domain="english-apps.us.auth0.com"
+        clientId="aHGX8oWUwcKnSf1vHjVNWfvkrEfhovcT"
+        redirectUri={window.location.origin}
+        audience="https://english-apps.us.auth0.com/api/v2/"
+        scope="read:current_user update:current_user_metadata"
+    >
+        <App/>
+    </Auth0Provider>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
